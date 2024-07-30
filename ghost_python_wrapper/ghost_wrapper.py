6f040dded4c38e5c9b6916110387014a21fbff7f
Samuel Coveney <scove003@plattsburgh.edu>
2024-07-29 21:18:12 -0400

    added function for basic auth, empty now

--- ghost_python_wrapper/ghost_wrapper.py
+++ ghost_python_wrapper/ghost_wrapper.py
@@ -1,5 +1,11 @@
 class GhostWrapper:
-    def __init__(self, host: str, key: str, version: str):
+    def __init__(self, host: str, version: str, username:str, password:str):
         self.host = host
-        self.key = key
         self.version = version
+        self.username = username
+        self.password = password
+
+        self.basic_auth()
+    
+    def basic_auth(self):
+        return
--- tests/test_ghost_wrapper.py
+++ tests/test_ghost_wrapper.py
@@ -1,12 +1,19 @@
 import pytest
+from unittest.mock import patch
 from ghost_python_wrapper.ghost_wrapper import GhostWrapper #I don't like this lol
 
 def test_always_passes():
     assert True
 
-def test_creates_object_properly():
-    new_ghost = GhostWrapper("http://localhost", "abc-123", "v5.0")
+def test_ghost_wrapper_creates_properly():
+    new_ghost = GhostWrapper("http://localhost", "v5.0", "jimmy", "qwerty123")
     assert new_ghost.host == "http://localhost"
-    assert new_ghost.key == "abc-123"
     assert new_ghost.version == "v5.0"
+    assert new_ghost.username == "jimmy"
+    assert new_ghost.password == "qwerty123"
+
+@patch('ghost_python_wrapper.ghost_wrapper.GhostWrapper.basic_auth')
+def test_ghost_wrapper_calls_basic_auth(mock_auth):
+    GhostWrapper("http://localhost", "v5.0", "jimmy", "qwerty123") #maybe make a fixture here
+    assert mock_auth.called
     
